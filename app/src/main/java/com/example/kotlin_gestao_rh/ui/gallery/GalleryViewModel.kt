b494387d33fbab3af6210183a97b725fc45dcdeb
igor-santos-19 <igoer174@gmail.com>
2024-09-22 14:59:17 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/kotlin_gestao_rh/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_gestao_rh.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
